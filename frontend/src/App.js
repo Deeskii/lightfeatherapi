54c38592e78ff133d57703d59c81a319c586669d
Dee Ragoo <deeskii32443@gmail.com>
2022-06-29 23:55:08 +0000

    frontend and backend init

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Lightfeather Coding Challenge
+        </p>
+        <a
+          className="App-link"
+          href="https://lightfeather.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Lightfeather
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
